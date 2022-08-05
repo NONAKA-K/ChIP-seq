07f07a8f6b0c60983d41f0bc3bb49df2dd719806
NONAKA-K <110658973+NONAKA-K@users.noreply.github.com>
2022-08-05 16:28:22 +0900

    v0.1 scripts for analysing ChIP-seq

---
+++ chip-seq-v0.1.sh
@@ -0,0 +1,25 @@
+#code of converting fastq to bam
+#v0.1 20220805
+#Nonaka Kazuki
+#Nagoya University Graduate school of science
+
+ref=/home/takumi/out/GCA_000146045.2_R64_genomic.fna
+for file in `ls *_1.fastq.gz`
+do
+    R1=$file
+    R2=${file%_1.fastq.gz}_2.fastq.gz
+    RP1=${file%_1.fastq.gz}_1fastp.fastq.gz
+    RP2=${file%_1.fastq.gz}_2fastp.fastq.gz
+    report=${file%_1.fastq.gz}.html
+    out=${file%_1.fastq.gz}.bam
+    fastp -i $R1 -I $R2 -o $RP1 -O $RP2 -h $report
+    bwa mem -t8 $ref $RP1 $RP2 | samtools sort -@ 4 > $out 
+    samtools index $out
+done
+
+#bwa mapping 
+#bwa mem -t8 $ref $R1 $R2 | samtools sort -@ 4 > SRR14808558.bam
+#sam to bam convert
+#samtools sort -@ 4 SRR14808558.sam > SRR14808558.bam
+#indexing
+#samtools index SRR14808558.bam
